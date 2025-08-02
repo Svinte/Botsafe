6afe40cd2b2e14011c78587036cd2a9c8e3b0ff7
Svinte <99759858+Svinte@users.noreply.github.com>
2025-08-02 22:27:19 +0300

    v1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
+<section style="display: flex; gap: 8px; justify-content: flex-end;">
+    <img src="https://raw.githubusercontent.com/github/explore/main/topics/vue/vue.png" alt="Vue.js" height="24" />
+    <img src="https://raw.githubusercontent.com/github/explore/main/topics/typescript/typescript.png" alt="TypeScript" height="24" />
+</section>
+
+
 # Botsafe
 Scripts to prevent Web scraping
+
+---
+
+## How it defends data?
+Botsafe breaks link content into array of letters. The data is stored into HTML link element via data attribute named ``data-botsafe``. Browser turns link data into working link using JavaScript.
+
+This kind of approach defends link data from Crawlers that:
+1. Have JavaScript disabled for security reasons.
+2. Run after DOM load before Botsafe starts.
+3. Are not used to crawl this kind of data.
+
+### Against AI crawlers
+We add noise into the array so regular AI cannot detect the pattern of the array. By default i2 and i5 are random noise.
+
+### Other defending tools.
+Botsafe allows every kind of HTML5 eventlistener to trigger Botsafe. You can change triggering method using link element ``data-reassemble-on`` -attribute.
+
+After finishing raessembling the Botsafe removes it's attributes not including botsafe classname.
---
+++ Scripts/_botsafe.ts
@@ -0,0 +1,96 @@
+/**
+ * Checks if string is phone number.
+ *
+ * @param text String
+ * @returns Is phone number.
+ */
+function isPhoneNumber(text: string): boolean {
+    const phoneRegex =/^\+?[0-9\s]{7,}$/;
+
+    return phoneRegex.test(text);
+}
+
+/**
+ * Checks if string is email.
+ *
+ * @param text String
+ * @returns Is email.
+ */
+function isEmail(text: string): boolean {
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+    return emailRegex.test(text);
+}
+
+
+/**
+ * Set event listeners for links.
+ */
+function botsafeInit(): void {
+    const botsafeElements = document.querySelectorAll(".botsafe");
+
+    console.log(botsafeElements);
+
+    // Add reassembler event listener to link.
+    botsafeElements.forEach((element) => {
+        if (!(element instanceof HTMLAnchorElement)) return;
+
+        const reassembleOn = element.getAttribute("data-reassemble-on");
+
+        if (reassembleOn === 'load') {
+            reassemble.call(element, new Event('synthetic'));
+
+            return;
+        }
+
+        if (reassembleOn !== null) {
+            element.addEventListener(reassembleOn, reassemble);
+        }
+
+        // Disable use before reassemble.
+        addEventListener("click", (event) => {
+            event.preventDefault();
+
+            const linkElement = element as HTMLAnchorElement;
+
+            if (!linkElement.href || linkElement.getAttribute("href") === "#") {
+                event.preventDefault();
+            }
+        })
+    })
+}
+
+/**
+ * Reassemble link content.
+ */
+function reassemble(this: HTMLElement, _event: Event): void {
+    const rawData = this.getAttribute("data-botsafe") || "[]";
+    const array = JSON.parse(rawData) as string[];
+
+    for (let i = 0; i < array.length; i++) {
+        if (i % 2 === 0 || i % 5 === 0) {
+            array[i] = '';
+        }
+    }
+
+    const text = array.join("").trim();
+
+    const linkElement = this as HTMLAnchorElement;
+
+    if (isPhoneNumber(text)) {
+        linkElement.href = `tel:${text}`;
+        linkElement.innerText = text;
+    } else if (isEmail(text)) {
+        linkElement.href = `mailto:${text}`;
+        linkElement.innerText = text;
+    } else {
+        linkElement.innerText = text;
+    }
+
+    this.removeAttribute("data-botsafe");
+    this.removeAttribute("data-reassemble-on");
+}
+
+
+// Run when DOM loaded.
+document.addEventListener("DOMContentLoaded", botsafeInit);
